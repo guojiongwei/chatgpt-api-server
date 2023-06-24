5e92692b985a9f086746c2bddcbc352f040fcea5
guojiongwei <g17637907938@163.com>
2023-06-24 10:19:25 +0800

    feat: 优化代码格式

--- chatgpt.js
+++ chatgpt.js
@@ -1,7 +1,7 @@
-const { Configuration, OpenAIApi  } = require('openai')
+const { Configuration, OpenAIApi } = require("openai");
 
 // 封装方法
-const writeStream =  async ({ stream, messages }) => {
+const writeStream = async ({ stream, messages }) => {
   const configuration = new Configuration({
     apiKey: process.env.OPENAI_API_KEY,
   });
@@ -9,39 +9,39 @@ const writeStream =  async ({ stream, messages }) => {
   const openai = new OpenAIApi(configuration);
 
   // 开启 stream 配置并设置 responseType
-  const completion = await openai.createChatCompletion({
-    messages,
-    stream: true,
-    model: 'gpt-3.5-turbo',
-  },  { responseType: 'stream' });
+  const completion = await openai.createChatCompletion(
+    {
+      messages,
+      stream: true,
+      model: "gpt-3.5-turbo",
+    },
+    { responseType: "stream" }
+  );
 
-  let isOne = true
+  let isOne = true;
   // 监听事件
-  completion.data.on('data', (data) => {
+  completion.data.on("data", (data) => {
     try {
       // 对每次推送的数据进行格式化, 得到的是 JSON 字符串、或者 [DONE] 表示流结束
-      const value = data.toString('utf8').trim()
-      const message = isOne ? value.split('\ndata: ')[1] : value.substr(6)
-      isOne = false
-	    console.log(message)
+      const value = data.toString("utf8").trim();
+      const message = isOne ? value.split("\ndata: ")[1] : value.substr(6);
+      isOne = false;
       // 流结束
-      if (message && message.endsWith('[DONE]')) {
-        stream.end()
+      if (message && message.endsWith("[DONE]")) {
+        stream.end();
         return;
       }
       // 解析数据
       const parsed = JSON.parse(message);
       const msg = parsed.choices[0].delta.content;
       // 写入流
-      stream.write(msg || '');
+      stream.write(msg || "");
     } catch (e) {
-      console.log(e.message)
-      // 出现错误, 结束
-     // stream.end()
+      console.log(e.message);
     }
   });
-}
+};
 
 module.exports = {
-  writeStream
-}
+  writeStream,
+};
--- index.js
+++ index.js
@@ -1,79 +1,39 @@
-const { PassThrough } = require('stream');
+const { PassThrough } = require("stream");
 
-const Koa = require('koa');
+const Koa = require("koa");
 const app = new Koa();
-const Router = require('koa-router');
+const Router = require("koa-router");
 const router = new Router();
-const bodyParser = require('koa-bodyparser');
-const cors = require('@koa/cors');
-const { writeStream } = require('./chatgpt.js')
-console.log(process.env.OPENAI_API_KEY)
+const bodyParser = require("koa-bodyparser");
+const cors = require("@koa/cors");
+const { writeStream } = require("./chatgpt.js");
 
 app.use(cors());
 app.use(bodyParser());
 
-router.get('/', async (ctx, next) => {
-  ctx.body = 'Hello World';
+router.get("/", async (ctx, next) => {
+  ctx.body = "Hello World";
 });
 
-router.post('/chat', async (ctx, next) => {
+router.post("/chat", async (ctx, next) => {
   const { messages } = ctx.request.body;
   const stream = new PassThrough();
   ctx.set({
-    'Connection': 'keep-alive',
-    'Cache-Control': 'no-cache',
-    'Content-Type': 'text/event-stream',
+    Connection: "keep-alive",
+    "Cache-Control": "no-cache",
+    "Content-Type": "text/event-stream",
   });
-//  console.log(messages)
   ctx.body = stream;
   ctx.status = 200;
 
   // 写入流: 调用 openai 往 stream 不断写入流
-  writeStream({ 
-    stream, 
-    messages
-  })
-})
+  writeStream({
+    stream,
+    messages,
+  });
+});
 
 app.use(router.routes());
 app.use(router.allowedMethods());
 
-app.listen(3000, () => console.log('server started: http://localhost:3000'));
-
-async function main() {
-	// 开启 stream 配置并设置 responseType
-const completion = await openai.createChatCompletion({
-  stream: true,
-  model: 'gpt-3.5-turbo',
-  messages: [{ role: 'user', content: 'js Map 类型怎么用' }],
-},  { responseType: 'stream' });
-
-// 监听事件
-completion.data.on('data', (data) => {
-  
-	// 对每次推送的数据进行格式化, 得到的是 JSON 字符串、或者 [DONE] 表示流结束
-  const message = data
-    .toString()
-    .trim()
-    .replace(/^data: /, '');
-
-  // 流结束
-  if (message === '[DONE]') {
-    console.log('流结束');
-    return;
-  }
-	
-  try {
-  
-  // 解析数据
-  const parsed = JSON.parse(message);
-
-  // 打印有效内容
-  console.log('=>', parsed.choices[0].delta.content);
-	} catch(err) {
-		//console.log(err)
-	}
-  });
-}
-
-// main()
+app.listen(3000, () => console.log("server started: http://localhost:3000"));
